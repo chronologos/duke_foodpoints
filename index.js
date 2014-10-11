84b1211ec20dcf6802767347dc0e3e0e9fb32726
howardc93 <howardc93@gmail.com>
2014-10-11 11:39:50 +0000

    laid out basic auth flow

--- index.js
+++ index.js
@@ -1,72 +1,165 @@
 var express = require('express')
+var session = require('express-session')
 var app = express();
 var request = require('request')
 var async = require('async')
 var cheerio = require('cheerio')
-var $ = cheerio.load('<h2 class="title">Hello world</h2>');
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
-$.html();
-app.use(express.static(__dirname + '/public'))
+var passport = require('passport');
+var bcrypt = require('bcrypt');
+var LocalStrategy = require('passport-local').Strategy;
 var port = process.env.PORT || 3000
+var token_broker = "https://oauth.oit.duke.edu/oauth/token.php"
+var duke_card_host = "https://dukecard-proxy.oit.duke.edu"
+app.use(express.static(__dirname + '/public'))
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: true,
+    resave: true
+}))
+app.use(passport.initialize())
+app.use(passport.session()) // persistent login;
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.listen(port, function() {
     console.log("Node app is running on port " + port)
 })
-app.get('/', function(req, res) {
-    root_url = req.protocol + '://' + req.get('host');
-    res.render('index.jade', {
-        auth_link: "https://oauth.oit.duke.edu/oauth/authorize.php?response_type=code&client_id=" + process.env.API_ID + "&state=xyz&scope=food_points&redirect_uri=" + root_url + "/home/auth"
-    })
-    //require https in production
-    //enforce email regex, password length
-    //
-    //http://studentaffairs.duke.edu/dining/venues-menus-hours
-    //@table=doc.css('#schedule_table')
-})
-app.get('/home/auth', function(req, res) {
-    console.log(req);
-    res.end()
-})
-/*
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(function(username, password, done) {
     User.findOne({
-        username: username
+        email: username
     }, function(err, user) {
         if(err) {
             return done(err);
         }
-        if(!user) {
-            return done(null, false, {
-                message: 'Incorrect username.'
-            });
-        }
-        if(!user.validPassword(password)) {
-            return done(null, false, {
-                message: 'Incorrect password.'
-            });
-        }
-        return done(null, user);
+        bcrypt.compare(password, user.password_hash, function(err, res) {
+            if(res) {
+                return done(null, user);
+            } else {
+                return done(null, false, {
+                    message: 'Login failure.'
+                });
+            }
+        });
     });
 }));
-//generate
-var bcrypt = require('bcrypt');
-bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash("B4c0/\/", salt, function(err, hash) {
-        // Store hash in your password DB.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
     });
 });
-//check
-// Load hash from your password DB.
-bcrypt.compare("B4c0/\/", hash, function(err, res) {
-    // res == true
-});
-bcrypt.compare("not_bacon", hash, function(err, res) {
-    // res == false
-});
+app.use(function(req, res, next) {
+    auth_url = req.protocol + '://' + req.get('host') + "/home/auth";
+    next();
+})
+app.get('/', function(req, res) {
+    res.render('index.jade', {
+        auth_link: "https://oauth.oit.duke.edu/oauth/authorize.php?response_type=code&client_id=" + process.env.API_ID + "&state=xyz&scope=food_points&redirect_uri=" + auth_url
+    })
+    //todo require https in production
+})
+app.get('/login', function(req, res) {
+    //todo
+})
+app.post('/login', function(req, res) {
+    //todo support sessions
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+})
+app.get('/register', function(req, res) {
+    //todo
+})
+app.post('/register', function(req, res) {
+    //todo enforce email regex, password length
+    /*
+         before_save { self.email = email.downcase }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence:   true,
+    format:     { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, length: { minimum: 6 }
+    */
+    var password = "test"
+    var salt = ""
+    bcrypt.hash(password, salt, function(err, hash) {
+        //todo store user
+        // Store hash in your password DB.
+    });
+})
+app.get('/home/auth', function(req, res) {
+    var code = req.query.code
+    console.log(code)
+    request.post(token_broker, {
+        auth: {
+            'user': process.env.API_ID,
+            'pass': process.env.API_SECRET
+        },
+        form: {
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: auth_url
+        }
+    }, function(err, resp, body) {
+        body = JSON.parse(body)
+        console.log(body)
+        //we only really care about the refresh token, which is valid for 6 months
+        //persist it in db and use to retrieve balances automatically
+        //get a single balance now to show the user
+        getBalance(body.refresh_token, function(err, bal) {
+            console.log(bal)
+        })
+        res.redirect("/")
+    })
+})
+
+function getBalance(refresh_token, cb) {
+    //TODO handle error gracefully when the refresh token has expired
+    request.post(token_broker, {
+        auth: {
+            'user': process.env.API_ID,
+            'pass': process.env.API_SECRET
+        },
+        form: {
+            grant_type: "refresh_token",
+            refresh_token: refresh_token
+        }
+    }, function(err, resp, body) {
+        console.log(body)
+        body = JSON.parse(body)
+        //use the new access token
+        var access_token = body.access_token
+        request.post(duke_card_host + "/food_points", {
+            form: {
+                access_token: access_token
+            }
+        }, function(err, resp, body) {
+            console.log(body)
+            body = JSON.parse(body)
+            cb(null, body.food_points)
+        })
+    })
+}
+
+//setInterval(updateBalances, 5000)
+function updateBalances(){
+    //todo function to continuously update balances
+    //loop through all users in db
+    getBalance(user.refresh_token, function(err, bal){
+        console.log(bal)
+    })
+}
+app.get('/whatsopen', function(req, res) {
+    request("http://studentaffairs.duke.edu/dining/venues-menus-hours", function(err, resp, body) {
+        var $ = cheerio.load(body)
+        res.send($.html("#schedule_table"))
+    })
+})
+/*
 var http = require('http');
 var pg = require('pg');
 var conString = "postgres://username:password@localhost/database";
@@ -107,92 +200,4 @@ var server = http.createServer(function(req, res) {
     });
 })
 server.listen(3001)
-
-class User < ActiveRecord::Base
-    before_save { self.email = email.downcase }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence:   true,
-    format:     { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, length: { minimum: 6 }
-
-    APP_CONFIG = {
-        token_broker_host: "https://oauth.oit.duke.edu",
-        token_request_path: "/oauth/token.php",
-        test_consumer_key: "foodpointsplus",
-        test_consumer_secret: ENV["CONSUMER_SECRET"],
-        # DukeCard proxy service url
-        duke_card_host: "https://dukecard-proxy.oit.duke.edu/"
-        }
-
-    def store_refresh_token(code)
-        url = URI.escape APP_CONFIG[:token_broker_host] + APP_CONFIG[:token_request_path]
-        uri = URI.parse(url)
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Post.new(uri.request_uri)
-        request.basic_auth(APP_CONFIG[:test_consumer_key], APP_CONFIG[:test_consumer_secret])  
-        request.set_form_data({"grant_type" => 'authorization_code', "code" => code, "redirect_uri" =>root_url+"home/auth"})
-        request.add_field "Accept", "application/json"
-        logger.info("fetching token #{code} against #{url}")
-        resp = http.request(request)
-        jsonResp = ActiveSupport::JSON.decode(resp.body)
-
-        # Now jsonResp will look like this:
-        # {"access_token"=>"739bd0c1a47a5723626f6b68109ab11940d2e9fb", 
-        #   "expires_in"=>3600, 
-        #   "token_type"=>"bearer", 
-        #   "scope"=>"flex", 
-        #   "refresh_token"=>"9ae29ed202a0e4be5a0a7bc972ef9d9a33baceb5"}
-
-        puts jsonResp
-
-        self.refresh_token = jsonResp["refresh_token"]
-    end
-
-    def update_balance
-
-        if self.refresh_token.nil?
-            return
-        end
-
-        # curl -u 'ClientId:ClientSecret' https://oauth.oit.duke.edu/oauth/token.php -d 'grant_type=refresh_token&refresh_token=<VALUE>'
-
-        #TODO handle error gracefully when the refresh token has expired
-
-        url = URI.escape APP_CONFIG[:token_broker_host] + APP_CONFIG[:token_request_path]
-        uri = URI.parse(url)
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Post.new(uri.request_uri)
-        request.basic_auth(APP_CONFIG[:test_consumer_key], APP_CONFIG[:test_consumer_secret])  
-        request.set_form_data({"grant_type" => 'refresh_token', "refresh_token" => self.refresh_token})
-        request.add_field "Accept", "application/json"
-        resp = http.request(request)
-        jsonResp = ActiveSupport::JSON.decode(resp.body)
-
-        puts jsonResp
-
-        token = jsonResp["access_token"]
-        url = URI.escape APP_CONFIG[:duke_card_host] + 'food_points'  # Here I am calling the dukecard flex menthod.  Use food_points for food points
-        uri = URI.parse(url)
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE  # The certificate is currently self-signed due to Heartbleed.  You can remove this line next week probably
-
-        request = Net::HTTP::Post.new(uri.request_uri)
-        request.set_form_data({"access_token" => token})
-        request.add_field "Accept", "application/json"
-        logger.info("calling duke card food_points with token #{token} to #{url}")
-
-        resp = http.request(request)
-        Balance.new(user_id=>current_user.id, balance=>resp["food_points"])
-    end
-end
 */
