43659fa5841b55e423386236dcf7afae30d76882
howardc93 <howardc93@gmail.com>
2014-10-16 23:59:06 +0000

    working auth and balance display

--- index.js
+++ index.js
@@ -1,47 +1,86 @@
 var dotenv = require('dotenv');
 dotenv.load();
 var express = require('express')
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session')
+var session = require('express-session');
+var SessionStore = require('connect-mongo')(session);
 var app = express();
 var request = require('request')
 var async = require('async')
 var cheerio = require('cheerio')
 var passwordless = require('passwordless');
 var MongoStore = require('passwordless-mongostore');
-var sendgrid = require("sendgrid")(process.env.SENDGRID_USER, process.env.SENDGRID_KEY);
+var sendgrid = require("sendgrid")(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+var router = express.Router();
 var port = process.env.PORT || 3000
 var token_broker = "https://oauth.oit.duke.edu/oauth/token.php"
 var duke_card_host = "https://dukecard-proxy.oit.duke.edu"
 var protocol = "http"
-var pathToMongoDb = process.env.MONGOHQ_URL
-//todo add monk
-//index user by email
-passwordless.init(new MongoStore(pathToMongoDb));
-app.use(bodyParser.json());
+var auth_url = process.env.ROOT_URL + "/home/auth";
+var db = require('monk')(process.env.MONGOHQ_URL)
+var users = db.get("users");
+var balances = db.get("balances");
+users.index('email', {
+    unique: true
+});
+passwordless.init(new MongoStore(process.env.MONGOHQ_URL));
+// Set up a delivery service
+passwordless.addDelivery(function(tokenToSend, uidToSend, recipient, callback) {
+    var host = process.env.ROOT_URL
+    var payload = {
+        text: 'Hello!\nAccess your account here: ' + host + '?token=' + tokenToSend + '&uid=' + encodeURIComponent(uidToSend),
+        from: "login@foodpointsplus.com",
+        to: recipient,
+        subject: 'Login for FoodPoints+'
+    }
+    sendEmail(payload, function(err) {
+        callback(err)
+    })
+})
+app.set('view engine', 'jade')
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser());
 app.use(session({
     secret: process.env.SESSION_SECRET,
+    store: new SessionStore({
+        url: process.env.MONGOHQ_URL
+    }),
     saveUninitialized: true,
     resave: true
-}))
+}));
 app.use(passwordless.sessionSupport());
 app.use(passwordless.acceptToken({
     successRedirect: '/'
 }));
 app.use(function(req, res, next) {
+    console.log(req.user)
     if(req.user) {
-        User.find({
+        users.update({
             email: req.user
-        }, function(error, user) {
-            res.locals.user = user;
-            next();
-        });
+        }, {
+            upsert: true
+        }, function(err) {
+            users.findOne({
+                email: req.user
+            }, function(err, user) {
+                console.log(user)
+                balances.find({
+                    email: user.email
+                }, function(err, bals) {
+                    user.balances = bals
+                    res.locals.user = user
+                    next();
+                })
+            })
+        })
     } else {
         next();
     }
 })
-app.set('view engine', 'jade')
 app.listen(port, function() {
     console.log("Node app is running on port " + port)
 })
@@ -56,32 +95,19 @@ if(process.env.NODE_ENV == "production") {
     app.use(forceSsl);
     protocol = "https";
 }
-var auth_url = process.env.ROOT_URL + "/home/auth";
-// Set up a delivery service
-passwordless.addDelivery(function(tokenToSend, uidToSend, recipient, callback) {
-    var host = process.env.ROOT_URL
-    var payload = {
-        text: 'Hello!\nAccess your account here: http://' + host + '?token=' + tokenToSend + '&uid=' + encodeURIComponent(uidToSend),
-        from: yourEmail,
-        to: recipient,
-        subject: 'Token for ' + host
-    }
-    sendEmail(payload, function(err) {
-        callback(err)
-    })
-})
 app.get('/', function(req, res) {
     res.render('index.jade', {
         auth_link: "https://oauth.oit.duke.edu/oauth/authorize.php?response_type=code&client_id=" + process.env.API_ID + "&state=xyz&scope=food_points&redirect_uri=" + auth_url
     })
 })
-app.post('/login', passwordless.requestToken(
+app.post('/sendtoken', passwordless.requestToken(
+    // Simply accept every user
+
     function(user, delivery, callback) {
-        console.log(user)
-        callback(ret.email)
+        callback(null, user);
     }), function(req, res) {
-    res.redirect("/");
-})
+    res.redirect("/")
+});
 app.get('/home/auth', function(req, res) {
     var code = req.query.code
     console.log(code)
@@ -100,15 +126,18 @@ app.get('/home/auth', function(req, res) {
         console.log(body)
         //we only really care about the refresh token, which is valid for 6 months
         //persist it in db and use to retrieve balances automatically
-        //get a single balance now to show the user
-        getBalance(body.refresh_token, function(err, bal) {
-            console.log(bal)
+        users.update({
+            email: req.user
+        }, {
+            $set: {
+                refresh_token: body.refresh_token
+            }
         })
-        res.redirect("/")
+        res.redirect('/')
     })
 })
 
-function getBalance(refresh_token, cb) {
+function getCurrentBalance(refresh_token, cb) {
     //TODO handle error gracefully when the refresh token has expired
     request.post(token_broker, {
         auth: {
@@ -131,31 +160,63 @@ function getBalance(refresh_token, cb) {
         }, function(err, resp, body) {
             console.log(body)
             body = JSON.parse(body)
-            cb(null, body.food_points)
+            cb(null, Number(body.food_points))
         })
     })
 }
-//setInterval(updateBalances, 5000)
+updateBalances()
 
 function updateBalances() {
+    console.log("updating balances")
     //todo function to continuously update balances
-    //loop through all users in db
+    //loop through all users in db with refresh tokens
     //for each user get their most recent balance
     //get a new balance for that user
     //only insert in db if number has changed
-    getBalance(user.refresh_token, function(err, bal) {
-        console.log(bal)
+    users.find({
+        refresh_token: {
+            $exists: true
+        }
+    }, function(err, res) {
+        async.mapSeries(res, function(user, cb) {
+            //get most recent balance
+            balances.findOne({
+                email: user.email
+            }, {
+                sort: {
+                    date: -1
+                }
+            }, function(err, bal) {
+                var mostRecent = 0
+                if(bal) {
+                    mostRecent = bal.balance
+                }
+                getCurrentBalance(user.refresh_token, function(err, bal) {
+                    //compare new balance to most recent balance
+                    console.log(mostRecent, bal)
+                    if(mostRecent != bal) {
+                        console.log("inserting new balance")
+                        balances.insert({
+                            email: user.email,
+                            balance: bal,
+                            date: Math.round(+new Date() / 1000)
+                        })
+                    }
+                    cb(null)
+                    //detect alert triggers
+                })
+            })
+        }, function(err) {
+            setTimeout(updateBalances, 10000)
+        })
     })
-    //todo power alerts with sendgrid
 }
 
-function sendEmail(payload) {
+function sendEmail(payload, cb) {
     console.log(payload)
     sendgrid.send(payload, function(err, json) {
-        if(err) {
-            console.error(err);
-        }
-        console.log(json);
+        console.log(json)
+        cb(err)
     });
 }
 app.get('/whatsopen', function(req, res) {
@@ -164,3 +225,9 @@ app.get('/whatsopen', function(req, res) {
         res.send($.html("#schedule_table"))
     })
 })
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
--- sample.js
+++ sample.js
@@ -5,19 +5,19 @@ db = conn.getDB("foodpoints");
 db.dropDatabase()
 
 //create test users
-db.user.insert({email: "howardc93@gmail.com", refresh_token:""})
+db.users.insert({email: "howardc93@gmail.com", refresh_token:""})
 //create test balances
-db.balance.insert({email: "howardc93@gmail.com", balance: 1490.95, date: 123456})
+db.balances.insert({email: "howardc93@gmail.com", balance: 1490.95, date: 123456})
 //create test budgets
-db.budget.insert({email: "howardc93@gmail.com", budget: 100, period: 1000})
+db.budgets.insert({email: "howardc93@gmail.com", budget: 100, period: 1000})
 
 //sample query, find all users
-cursor = db.user.find();
+cursor = db.users.find();
 while ( cursor.hasNext() ) {
    printjson( cursor.next() );
 }
 //find all balances for a user
-cursor = db.balance.find({email: "howardc93@gmail.com"});
+cursor = db.balances.find({email: "howardc93@gmail.com"});
 while ( cursor.hasNext() ) {
    printjson( cursor.next() );
 }
