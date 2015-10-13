1479545a80242c1336885fabcf86d06b9637b77f
Yi Yan <yiyantay@gmail.com>
2015-10-13 11:29:09 -0400

    reorganize NgFiles

---
+++ public/myNgApp/myNgApp.js
@@ -0,0 +1,55 @@
+angular.module('foodpoints', [])
+
+  .controller("AdvancedStatsController", function($scope) {
+    $scope.advanced = false;
+  })
+
+  .controller("AverageSpendingController", function($scope, $http) {
+    $http.get('/api/spending')
+      .then(function(response) {
+        $scope.average = parseFloat(response.data).toFixed(2);
+      }
+  );
+})
+
+  .controller("BudgetController", function($scope, $http) {
+    $http.get('/api/cutoffs/')
+      .success(function(data, status, headers, config) {
+        $scope.periods = Object.keys(data);
+        $scope.budget = {
+          amount: 150,
+          period: 'week'
+        };
+      });
+    getBudgets($scope, $http);
+    $scope.save = function(budget) {
+      console.log(budget);
+      $http.post('/api/budgets/', budget)
+        .success(function(data, status, headers, config) {
+          console.log(data);
+          getBudgets($scope, $http);
+        });
+    };
+    $scope.delete = function(budget) {
+      $http.delete('/api/budgets/' + budget._id)
+        .success(function(data, status, headers, config) {
+          console.log(data);
+          getBudgets($scope, $http);
+        });
+    };
+  });
+
+  function getBudgets($scope, $http) {
+      $http.get('/api/budgets/').
+      success(function(data, status, headers, config) {
+          data.forEach(function(b) {
+              b.percent = Math.min(b.spent / b.amount * 100, 100);
+              b.elapsed = moment().diff(b.cutoff) / moment.duration(1, b.period).asMilliseconds() * 100;
+              var classes = ["progress-bar-success", "progress-bar", "progress-bar-striped", "active"];
+              classes[0] = b.percent > b.elapsed ? "progress-bar-warning" : classes[0];
+              b.class = classes.join(" ");
+              b.display = b.spent.toFixed() + " of " + b.amount + " this " + b.period;
+          });
+          $scope.budgets = data;
+      });
+  }
