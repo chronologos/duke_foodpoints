07c0473bef4c917b28066b874637007735297b64
Yi Yan <yiyantay@gmail.com>
2015-10-13 18:45:24 -0400

    cosmetic changes and angular struggles
    Signed-off-by: Yi Yan <yiyantay@gmail.com>

--- public/myNgApp/MealPlanController.js
+++ public/myNgApp/MealPlanController.js
@@ -1,5 +1,5 @@
 angular.module('foodpoints')
-.controller("MealPlanController", function($scope){
+.controller("MealPlanController", ['$scope',function($scope){
   $scope.mealPlans=[{
     name: "Plan H ($432, Freshmen)",
     value: 432
@@ -38,30 +38,35 @@ angular.module('foodpoints')
     }
   ];
 
-  // a long chunk of code that essentially stores your selected mealplan in a cookie and uses it to update progbars
-  if (docCookies.getItem("foodplan")!==null){
-    $scope.selectedItemName = docCookies.getItem("foodplan");
-    $scope.mealPlanCost = docCookies.getItem("numfoodpoints");
-    var percent2 = Math.min($("#balance").text() / $scope.mealPlanCost, 1);
-    $("#progbar2").width(percent2 * 100 + "%");
-    if (user && chart) {
-        chart.load({
-            columns: [
-                ['Ideal', $scope.mealPlanCost, 0]
-            ]
-        });
+// a long chunk of code that essentially stores your selected mealplan in a cookie and uses it to update progbars
+  $scope.init = function () {
+    if (docCookies.getItem("foodplan")!==null){
+      $scope.selectedItemName = docCookies.getItem("foodplan");
+      $scope.mealPlanCost = docCookies.getItem("numfoodpoints");
+      var percent2 = Math.min($("#balance").text() / $scope.mealPlanCost, 1);
+      console.log(percent2);
+      $("#progbar2").width(percent2 * 100 + "%");
+      if (user && chart) {
+          chart.load({
+              columns: [
+                  ['Ideal', $scope.mealPlanCost, 0]
+              ]
+          });
+      }
+      numfoodpoints = $scope.mealPlanCost; //necessary cos foodpoints.js does updates progress bar text with this
     }
-    numfoodpoints = $scope.mealPlanCost; //necessary cos foodpoints.js does updates progress bar text with this
-  }
-  else {
-    $scope.selectedItemName = "Choose meal plan";
-  }
+    else {
+      $scope.selectedItemName = "Choose meal plan";
+    }
+  };
+
   // dynamically change progressbar size on change in food plan
   $scope.dropboxitemselected = function (thisItem) {
       $scope.selectedItemName = thisItem.name;
       $scope.mealPlanCost = thisItem.value;
       // alert($scope.selectedItemName);
       var percent2 = Math.min($("#balance").text() / $scope.mealPlanCost, 1);
+
       $("#progbar2").width(percent2 * 100 + "%");
 
       //store numfoodpoints and foodplan as cookies
@@ -77,4 +82,20 @@ angular.module('foodpoints')
       }
       numfoodpoints = $scope.mealPlanCost; //necessary cos foodpoints.js does updates progress bar text with this
   };
-});
+}])
+.directive('ngwatchthis',['$interval', function($interval) {
+  return {
+    link: function(scope, element, attrs) {
+       scope.$watch("ngwatchthis", function() {
+         if (!isNaN(parseInt(element.html()))){
+           console.log(parseInt(element.html())+".")
+         }
+         
+        var a = setTimeout(function(){console.log("delayed " + element.html()+" "+ typeof(element.html()))},1000);
+       });
+    }
+  };
+
+  }
+
+]);
