97f63022082e5461555c12c67478938c0e97d3db
Adithya93 <ra102@duke.edu>
2015-10-25 03:23:45 -0400

    Hall of Fame for top N items

--- public/myNgApp/UserController.js
+++ public/myNgApp/UserController.js
@@ -19,6 +19,8 @@ angular.module('foodpoints')
           //$scope.user.fav = parseFloat(Math.abs(fav)).toFixed(2);
           $scope.user.fav = "" + parseFloat(Math.abs(favInfo[0])).toFixed(2);
           $scope.user.numFav = "" + favInfo[1];
+          $scope.user.allFavs = getNFavs(5, trans);
+
 
           console.log("User's favorite item costs " + $scope.user.fav + " and it was bought " + $scope.user.numFav + " times");
       }
@@ -54,9 +56,25 @@ angular.module('foodpoints')
     return arr;
   }
 
+// Used by both getFav and getNFavs
+function getFreqs(trans) {
+  console.log("Type of input to getFreqs method: " + typeof(trans));
+  var freqs = {};
+  trans.forEach(function(x){
+    if (!freqs["" + x.amount]) {
+      freqs["" + x.amount] = 1;
+    }
+    else {
+      freqs["" + x.amount] ++;
+    }
+  });
+  return freqs;
+}
+
 // Naive O(N) implementation
   function getFav(trans) {
 
+    /***
     console.log("Type of input to getFav method: " + typeof(trans));
     var freqs = {};
     trans.forEach(function(x){
@@ -67,10 +85,96 @@ angular.module('foodpoints')
         freqs["" + x.amount] ++;
       }
     });
-
+    ***/
+    var freqs = getFreqs(trans);
     var fav;
     var maxCount = 0;
     Object.keys(freqs).forEach(function(x){if (freqs[x] > maxCount){maxCount = freqs[x]; fav = x;}});
     console.log("Fav is " + fav + " which was bought " + maxCount + " times");
     return [fav, maxCount];
   }
+
+// Temporary... Tried to avoid O(N^2) but failed miserably cos Javascript objects do not preserve key order :(
+  function getNFavs(n, trans) {
+    var freqs = getFreqs(trans);
+    var keys = Object.keys(freqs);
+    //var result = {};
+    var topKeys = [];
+    var result = []
+    if (keys.length < n) {
+      var sortedVals = keys.map(function(val, pos) {
+        return freqs[val];
+      }).sort().reverse();
+      sortedVals.forEach(function(x) {
+        keys.forEach(function(y) {
+          if (freqs[y] === x && topKeys.indexOf(y) === -1) {
+          //  result[y] = x;
+            topKeys.push(y);
+          }
+        })
+      });
+      //return result;
+      for (var i = 0; i < topKeys.length; i ++) {
+        result.push([topKeys[i], sortedVals[i]]);
+      }
+      //return [topKeys, sortedVals];
+      return result;
+    }
+    var firstKeys = keys.slice(0, n);
+    var topFreqs = firstKeys.map(function(val, pos) {
+      return freqs[val];
+    }).sort().reverse();
+    keys.slice(n).forEach(function(x) {
+      if (freqs[x] > topFreqs[n-1]) {
+        topFreqs.push(freqs[x]);
+        firstKeys.push(x);
+        topFreqs.sort().reverse();
+        topFreqs.pop();
+      //  var removed = topFreqs.pop();
+      //  topKeys.forEach(function(y) {
+      //for (var index = 0; index < topKeys.length; index ++) {
+      //    if (freqs[topKeys[index]] === removed) {
+      //     top
+      //    }
+      //  })
+      //}
+      }
+    });
+    var done = 0;
+      topFreqs.forEach(function(x) {
+      firstKeys.forEach(function(y) {
+        if (freqs[y] === x && topKeys.indexOf(y) === -1 && done < n) {
+        //  result[y] = x;
+          topKeys.push(y);
+          console.log("Added key " + y + " with value " + x);
+          done ++;
+        }
+      });
+    });
+//      return [topKeys, topFreqs];
+      for (var i = 0; i < topKeys.length; i ++) {
+        result.push([topKeys[i], topFreqs[i]]);
+      }
+      //return [topKeys, sortedVals];
+      return result;
+
+    };
+
+/***
+// Sort Object by Value, returning array of 2 arrays with keys in one and values in another
+    function sortObj(obj) {
+      var keys = Object.keys(obj);
+      var sortedVals = keys.map(function(val, pos) {
+        return obj[val];
+      }).sort().reverse();
+      var rightKeys = [];
+      sortedVals.forEach(function(x) {
+        keys.forEach(function(y) {
+          if (obj[y] === x && rightKeys.indexOf(y) === -1) {
+            rightKeys.push(y);
+          }
+        });
+      });
+      return [rightKeys, sortedVals];
+    }
+    ***/
