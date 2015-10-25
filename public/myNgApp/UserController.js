3b4c2286e11d4fb34d94582f7ebd5dba810e93a7
Adithya93 <ra102@duke.edu>
2015-10-25 09:58:18 -0400

    List of favorite transactions

--- public/myNgApp/UserController.js
+++ public/myNgApp/UserController.js
@@ -14,13 +14,13 @@ angular.module('foodpoints')
           var trans = getTrans($scope.user.balances);
           console.log("Number of different transactions detected for client: " + trans.length);
 
-      //    var favInfo = getFav(trans);
+          var favInfo = getFav(trans);
 
           //$scope.user.fav = parseFloat(Math.abs(fav)).toFixed(2);
-      //    $scope.user.fav = "" + parseFloat(Math.abs(favInfo[0])).toFixed(2);
-      //    $scope.user.numFav = "" + favInfo[1];
+          $scope.user.fav = "" + parseFloat(Math.abs(favInfo[0])).toFixed(2);
+          $scope.user.numFav = "" + favInfo[1];
 
-      //    console.log("User's favorite item costs " + $scope.user.fav + " and it was bought " + $scope.user.numFav + " times");
+          console.log("User's favorite item costs " + $scope.user.fav + " and it was bought " + $scope.user.numFav + " times");
       }
       else console.log("Error"+status);
     });
@@ -54,26 +54,9 @@ angular.module('foodpoints')
     return arr;
   }
 
-/***
-// Used by both getFav and getNFavs
-function getFreqs(trans) {
-  console.log("Type of input to getFreqs method: " + typeof(trans));
-  var freqs = {};
-  trans.forEach(function(x){
-    if (!freqs["" + x.amount]) {
-      freqs["" + x.amount] = 1;
-    }
-    else {
-      freqs["" + x.amount] ++;
-    }
-  });
-  return freqs;
-}
-***/
 // Naive O(N) implementation
   function getFav(trans) {
 
-    
     console.log("Type of input to getFav method: " + typeof(trans));
     var freqs = {};
     trans.forEach(function(x){
@@ -84,85 +67,10 @@ function getFreqs(trans) {
         freqs["" + x.amount] ++;
       }
     });
-    
-//    var freqs = getFreqs(trans);
+
     var fav;
     var maxCount = 0;
     Object.keys(freqs).forEach(function(x){if (freqs[x] > maxCount){maxCount = freqs[x]; fav = x;}});
     console.log("Fav is " + fav + " which was bought " + maxCount + " times");
     return [fav, maxCount];
   }
-
-
-
-
-
-/***
-
-
-// Temporary... Tried to avoid O(N^2) but failed miserably cos Javascript objects do not preserve key order :(
-  function getNFavs(n, trans) {
-    var freqs = getFreqs(trans);
-    var keys = Object.keys(freqs);
-    //var result = {};
-    var topKeys = [];
-    var result = []
-    if (keys.length < n) {
-      var sortedVals = keys.map(function(val, pos) {
-        return freqs[val];
-      }).sort().reverse();
-      sortedVals.forEach(function(x) {
-        keys.forEach(function(y) {
-          if (freqs[y] === x && topKeys.indexOf(y) === -1) {
-          //  result[y] = x;
-            topKeys.push(y);
-          }
-        })
-      });
-      //return result;
-      for (var i = 0; i < topKeys.length; i ++) {
-        result.push([topKeys[i], sortedVals[i]]);
-      }
-      //return [topKeys, sortedVals];
-      return result;
-    }
-    var firstKeys = keys.slice(0, n);
-    var topFreqs = firstKeys.map(function(val, pos) {
-      return freqs[val];
-    }).sort().reverse();
-    keys.slice(n).forEach(function(x) {
-      if (freqs[x] > topFreqs[n-1]) {
-        topFreqs.push(freqs[x]);
-        firstKeys.push(x);
-        topFreqs.sort().reverse();
-        topFreqs.pop();
-      //  var removed = topFreqs.pop();
-      //  topKeys.forEach(function(y) {
-      //for (var index = 0; index < topKeys.length; index ++) {
-      //    if (freqs[topKeys[index]] === removed) {
-      //     top
-      //    }
-      //  })
-      //}
-      }
-    });
-    var done = 0;
-      topFreqs.forEach(function(x) {
-      firstKeys.forEach(function(y) {
-        if (freqs[y] === x && topKeys.indexOf(y) === -1 && done < n) {
-        //  result[y] = x;
-          topKeys.push(y);
-          console.log("Added key " + y + " with value " + x);
-          done ++;
-        }
-      });
-    });
-//      return [topKeys, topFreqs];
-      for (var i = 0; i < topKeys.length; i ++) {
-        result.push([topKeys[i], topFreqs[i]]);
-      }
-      //return [topKeys, sortedVals];
-      return result;
-
-    };
-    ***/
