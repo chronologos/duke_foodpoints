a401396a0d189f2148bb017da758df837a887dc5
Yi Yan <yiyantay@gmail.com>
2015-11-03 10:31:24 -0500

    code beautification, loading of resources

--- src/myNgApp/HeatMapController.js
+++ src/myNgApp/HeatMapController.js
@@ -1,110 +1,113 @@
 angular.module('foodpoints')
-.controller("HeatMapController", function($scope,$http,infoFactory,UserService){
-  var bucketSize = 5;
-  var numBuckets = 4;
-  var buckets = {};
-  var dayHeatmap = {};
-  var hourHeatMap = {};
-  var info = infoFactory.getInfo();
-  // set $scope.user to resolve the promise returned by UserService.User
-  $scope.fetchUser = function() {
-    UserService.User.then(function(results){
-      $scope.user = results;
-    });
-  };
-  $scope.fetchUser();
-  $scope.user = {};
-  // wait for promise to resolve
-  $scope.$watch('user', function(newVal, oldVal){
-  // fetching user from server takes a while, so we want to watch this for change and broadcast on change
-    if(newVal!=oldVal){
-      $scope.$broadcast('userChange',{"val":newVal});
-      console.log(newVal);
-      runBody();
-    }
-  });
-  function runBody(){
-    $scope.user.trans.forEach(function(exp) {
-      if (exp.amount < 0) {
-        var timestamp = moment(exp.date).format('X');
-        var timestamp2 = moment().startOf('day').hours(moment(exp.date).hours()).format('X');
-        var amt = exp.amount * -1;
-        dayHeatmap[timestamp] = ~~amt;
-        if (!hourHeatMap[timestamp2]) {
-          hourHeatMap[timestamp2] = 0;
-        }
-        hourHeatMap[timestamp2] += ~~amt;
-        for (var i = 0; i <= numBuckets; i++) {
-          //console.log(amt, i*bucketSize, (i+1)*bucketSize)
-          if (i >= numBuckets || (amt > i * bucketSize && amt <= (i + 1) * bucketSize)) {
-            //console.log(i, amt)
-            buckets[i * bucketSize] ? buckets[i * bucketSize] += amt : buckets[i * bucketSize] = amt;
-            break;
-          }
-        }
+  .controller("HeatMapController", function($scope, $http, infoFactory, UserService) {
+    var bucketSize = 5;
+    var numBuckets = 4;
+    var buckets = {};
+    var dayHeatmap = {};
+    var hourHeatMap = {};
+    var info = infoFactory.getInfo();
+    // set $scope.user to resolve the promise returned by UserService.User
+    $scope.fetchUser = function() {
+      UserService.User.then(function(results) {
+        $scope.user = results;
+      });
+    };
+    $scope.fetchUser();
+    $scope.user = {};
+    // wait for promise to resolve
+    $scope.$watch('user', function(newVal, oldVal) {
+      // fetching user from server takes a while, so we want to watch this for change and broadcast on change
+      if (newVal != oldVal) {
+        $scope.$broadcast('userChange', {
+          "val": newVal
+        });
+        console.log(newVal);
+        runBody();
       }
     });
-    console.log(JSON.stringify(dayHeatmap));
-    var cal = new CalHeatMap();
-    cal.init({
-      // itemSelector: "#animationDuration-a", //new
-      // domain: "day", //new
-      itemSelector: "#days",
-      start: info.fallstart,
-      range: 3,
-      previousSelector: "#animationDuration-previous",
-      nextSelector: "#animationDuration-next",
-      itemNamespace: "animationDuration-a",
-      domain: "month",
-      subDomain: "day",
-      highlight: new Date(),
-      data: dayHeatmap,
-      tooltip: true,
-      itemName: ["", ""],
-      subDomainTextFormat: function(date, value) {
-        return value ? value.toFixed() : "";
-      },
-      legendColors: {
-        min: "#fcffa3",
-        max: "#510d63",
-        empty: "white"
-        // Will use the CSS for the missing keys
-      },
-      legendVerticalPosition: "center",
-      legendOrientation: "vertical",
-      cellSize: 16
-    });
-    var cal2 = new CalHeatMap();
-    cal2.init({
-      itemSelector: "#hours",
-      start: new Date(),
-      range: 1,
-      domain: "day",
-      subDomain: "hour",
-      domainLabelFormat: "",
-      subDomainDateFormat: function(date) {
-        return moment(date).format("ha"); // Use the moment library to format the Date
-      },
-      cellPadding: 5,
-      verticalOrientation: true,
-      colLimit: 12,
-      tooltip: true,
-      data: hourHeatMap,
-      itemName: ["", ""],
-      subDomainTextFormat: function(date, value) {
-        return value ? value.toFixed() : "";
-      },
-      legendColors: {
-        min: "#fcffa3",
-        max: "#510d63",
-        empty: "white"
-        // Will use the CSS for the missing keys
-      },
-      cellSize: 16
-    });
+
+    function runBody() {
+      $scope.user.trans.forEach(function(exp) {
+        if (exp.amount < 0) {
+          var timestamp = moment(exp.date).format('X');
+          var timestamp2 = moment().startOf('day').hours(moment(exp.date).hours()).format('X');
+          var amt = exp.amount * -1;
+          dayHeatmap[timestamp] = ~~amt;
+          if (!hourHeatMap[timestamp2]) {
+            hourHeatMap[timestamp2] = 0;
+          }
+          hourHeatMap[timestamp2] += ~~amt;
+          for (var i = 0; i <= numBuckets; i++) {
+            //console.log(amt, i*bucketSize, (i+1)*bucketSize)
+            if (i >= numBuckets || (amt > i * bucketSize && amt <= (i + 1) * bucketSize)) {
+              //console.log(i, amt)
+              buckets[i * bucketSize] ? buckets[i * bucketSize] += amt : buckets[i * bucketSize] = amt;
+              break;
+            }
+          }
+        }
+      });
+      console.log(JSON.stringify(dayHeatmap));
+      var cal = new CalHeatMap();
+      cal.init({
+        // itemSelector: "#animationDuration-a", //new
+        // domain: "day", //new
+        itemSelector: "#days",
+        start: info.fallstart,
+        range: 3,
+        previousSelector: "#animationDuration-previous",
+        nextSelector: "#animationDuration-next",
+        itemNamespace: "animationDuration-a",
+        domain: "month",
+        subDomain: "day",
+        highlight: new Date(),
+        data: dayHeatmap,
+        tooltip: true,
+        itemName: ["", ""],
+        subDomainTextFormat: function(date, value) {
+          return value ? value.toFixed() : "";
+        },
+        legendColors: {
+          min: "#fcffa3",
+          max: "#510d63",
+          empty: "white"
+            // Will use the CSS for the missing keys
+        },
+        legendVerticalPosition: "center",
+        legendOrientation: "vertical",
+        cellSize: 16
+      });
+      var cal2 = new CalHeatMap();
+      cal2.init({
+        itemSelector: "#hours",
+        start: new Date(),
+        range: 1,
+        domain: "day",
+        subDomain: "hour",
+        domainLabelFormat: "",
+        subDomainDateFormat: function(date) {
+          return moment(date).format("ha"); // Use the moment library to format the Date
+        },
+        cellPadding: 5,
+        verticalOrientation: true,
+        colLimit: 12,
+        tooltip: true,
+        data: hourHeatMap,
+        itemName: ["", ""],
+        subDomainTextFormat: function(date, value) {
+          return value ? value.toFixed() : "";
+        },
+        legendColors: {
+          min: "#fcffa3",
+          max: "#510d63",
+          empty: "white"
+            // Will use the CSS for the missing keys
+        },
+        cellSize: 16
+      });
 
 
-  }
+    }
 
 
-});
+  });
--- src/myNgApp/myNgApp.js
+++ src/myNgApp/myNgApp.js
@@ -1,103 +1,101 @@
 angular.module('foodpoints', [])
 
-  .controller("AverageSpendingController", function($scope, $http) {
-    var globalDaily;
-    var globalWeekly;
-    $http.get('/api/spending')
-      .then(function(response) {
-        $scope.average = parseFloat(response.data).toFixed(2);
-        globalDaily = $scope.average;
-      }
-  );
-    $http.get('/api/personal')
-      .then(function(res) {
-        var checkDailyData = res.data;
-        var personalDaily = parseFloat(checkDailyData.day);
-        var personalWeekly = parseFloat(checkDailyData.week);
-        $scope.dailyTotal = isNaN(personalDaily) ? "Coming Soon!" : personalDaily.toFixed(2);
-        $scope.weeklyTotal = isNaN(personalWeekly) ? "Coming Soon!" : personalWeekly.toFixed(2);
-      }
-  );
-    $http.get('/api/spending/weekly')
-      .then(function(res) {
-        var weeklyTotal = res.data;
-        console.log("Response received for weekly total is " + weeklyTotal);
-        var weekTotalFloat = parseFloat(weeklyTotal);
-        globalWeekly = 7 * globalDaily;
-        var weekTotalFloat = isNaN(weekTotalFloat) ? globalWeekly : weekTotalFloat.toFixed(2);
-        $scope.wkAvg = weekTotalFloat;
-      });
+.controller("AverageSpendingController", function($scope, $http) {
+  var globalDaily;
+  var globalWeekly;
+  $http.get('/api/spending')
+    .then(function(response) {
+      $scope.average = parseFloat(response.data).toFixed(2);
+      globalDaily = $scope.average;
+    });
+  $http.get('/api/personal')
+    .then(function(res) {
+      var checkDailyData = res.data;
+      var personalDaily = parseFloat(checkDailyData.day);
+      var personalWeekly = parseFloat(checkDailyData.week);
+      $scope.dailyTotal = isNaN(personalDaily) ? "Coming Soon!" : personalDaily.toFixed(2);
+      $scope.weeklyTotal = isNaN(personalWeekly) ? "Coming Soon!" : personalWeekly.toFixed(2);
+    });
+  $http.get('/api/spending/weekly')
+    .then(function(res) {
+      var weeklyTotal = res.data;
+      console.log("Response received for weekly total is " + weeklyTotal);
+      var weekTotalFloat = parseFloat(weeklyTotal);
+      globalWeekly = 7 * globalDaily;
+      var weekTotalFloat = isNaN(weekTotalFloat) ? globalWeekly : weekTotalFloat.toFixed(2);
+      $scope.wkAvg = weekTotalFloat;
+    });
 })
 
-  .controller("BudgetController", function($scope, $http) {
-    $http.get('/api/cutoffs/')
-      .success(function(data, status, headers, config) {
-        $scope.periods = Object.keys(data);
-        $scope.budget = {
-          amount: 150,
-          period: 'week'
-        };
-      });
-    getBudgets($scope, $http);
-    $scope.save = function(budget) {
-      console.log(budget);
-      $http.post('/api/budgets/', budget)
-        .success(function(data, status, headers, config) {
-          console.log(data);
-          getBudgets($scope, $http);
-        });
-    };
-    $scope.delete = function(budget) {
-      $http.delete('/api/budgets/' + budget._id)
-        .success(function(data, status, headers, config) {
-          getBudgets($scope, $http);
-        });
-    };
-  })
-
-  // a factory with getter function to return commonly used constants
-  .factory('infoFactory', function(){
-    var service = {};
-    var currdate = new Date();
-    var acadyear = currdate.getMonth() > 6 ? currdate.getFullYear() : currdate.getFullYear() - 1;
-    var FALL_LENGTH = 16 * 7;
-    var SPRING_LENGTH = 16 * 7 + 4;
-    var fallstart = getFirstWeekday(1, 19, 7, acadyear); //monday after aug 19 of acad year
-    var fallend = addDays(fallstart, FALL_LENGTH);
-    var springstart = getFirstWeekday(3, 2, 0, acadyear + 1); //wednesday after jan 2 of following year
-    var springend = addDays(springstart, SPRING_LENGTH);
-    var fall = currdate > fallstart && currdate < fallend;
-    var spring = currdate > springstart && currdate < springend;
-    var semStart = spring ? springstart : fallstart;
-    var semEnd = spring ? springend : fallend;
-
-    service.getInfo = function(){
-      return {
-        "UPDATE_INTERVAL": 200,
-        "DEFAULT_FOOD_POINTS": 2152,
-        "MAX_AMOUNT_BALANCEADDITION": 1500,
-        "fall" : fall,
-        "spring": spring,
-        "start": semStart,
-        "end": semEnd,
-        "fallstart" : fallstart,
-        "fallend": fallend,
-        "springstart": springstart,
-        "springend": springend
+.controller("BudgetController", function($scope, $http) {
+  $http.get('/api/cutoffs/')
+    .success(function(data, status, headers, config) {
+      $scope.periods = Object.keys(data);
+      $scope.budget = {
+        amount: 150,
+        period: 'week'
       };
-    };
-    return service;
-  })
+    });
+  getBudgets($scope, $http);
+  $scope.save = function(budget) {
+    console.log(budget);
+    $http.post('/api/budgets/', budget)
+      .success(function(data, status, headers, config) {
+        console.log(data);
+        getBudgets($scope, $http);
+      });
+  };
+  $scope.delete = function(budget) {
+    $http.delete('/api/budgets/' + budget._id)
+      .success(function(data, status, headers, config) {
+        getBudgets($scope, $http);
+      });
+  };
+})
 
-  // service to return user and change user if necessary
-  .service('UserService', function($rootScope,$http,$q,infoFactory) {
-    var info = infoFactory.getInfo();
-    this.User = $http.get('/api/user')
+// a factory with getter function to return commonly used constants
+.factory('infoFactory', function() {
+  var service = {};
+  var currdate = new Date();
+  var acadyear = currdate.getMonth() > 6 ? currdate.getFullYear() : currdate.getFullYear() - 1;
+  var FALL_LENGTH = 16 * 7;
+  var SPRING_LENGTH = 16 * 7 + 4;
+  var fallstart = getFirstWeekday(1, 19, 7, acadyear); //monday after aug 19 of acad year
+  var fallend = addDays(fallstart, FALL_LENGTH);
+  var springstart = getFirstWeekday(3, 2, 0, acadyear + 1); //wednesday after jan 2 of following year
+  var springend = addDays(springstart, SPRING_LENGTH);
+  var fall = currdate > fallstart && currdate < fallend;
+  var spring = currdate > springstart && currdate < springend;
+  var semStart = spring ? springstart : fallstart;
+  var semEnd = spring ? springend : fallend;
+
+  service.getInfo = function() {
+    return {
+      "UPDATE_INTERVAL": 200,
+      "DEFAULT_FOOD_POINTS": 2152,
+      "MAX_AMOUNT_BALANCEADDITION": 1500,
+      "fall": fall,
+      "spring": spring,
+      "start": semStart,
+      "end": semEnd,
+      "fallstart": fallstart,
+      "fallend": fallend,
+      "springstart": springstart,
+      "springend": springend
+    };
+  };
+  return service;
+})
+
+// service to return user and change user if necessary
+.service('UserService', function($rootScope, $http, $q, infoFactory) {
+  var info = infoFactory.getInfo();
+  this.User = $http.get('/api/user')
     .then(function(res) {
       console.log("Data fetched using UserService Factory");
       return res.data;
     })
-    .then(function(res){
+    .then(function(res) {
       res.refresh_token_expire = format(res.refresh_token_expire);
       res.balance = res.balances[0].balance.toFixed(2);
       //filter out balances from previous semesters
@@ -106,57 +104,57 @@ angular.module('foodpoints', [])
       });
       // parse balance list into list of transactions and favourites
       res.trans = getTrans(res.balances);
-      res.favList = getFav(5,res.trans);
+      res.favList = getFav(5, res.trans);
       return res;
 
     });
 
-    this.UserChange = function(newUserObj){
-      this.User = newUserObj;
-    };
+  this.UserChange = function(newUserObj) {
+    this.User = newUserObj;
+  };
 
-  });
+});
 
 // HELPER FUNCTIONS
 // ----------------
 function getBudgets($scope, $http) {
-    $http.get('/api/budgets/').
-    success(function(data, status, headers, config) {
-        data.forEach(function(b) {
-            b.percent = Math.min(b.spent / b.amount * 100, 100);
-            b.elapsed = moment().diff(b.cutoff) / moment.duration(1, b.period).asMilliseconds() * 100;
-            var classes = ["progress-bar-success", "progress-bar", "progress-bar-striped", "active"];
-            classes[0] = b.percent > b.elapsed ? "progress-bar-warning" : classes[0];
-            b.class = classes.join(" ");
-            b.display = b.spent.toFixed() + " of " + b.amount + " this " + b.period;
-        });
-        $scope.budgets = data;
+  $http.get('/api/budgets/').
+  success(function(data, status, headers, config) {
+    data.forEach(function(b) {
+      b.percent = Math.min(b.spent / b.amount * 100, 100);
+      b.elapsed = moment().diff(b.cutoff) / moment.duration(1, b.period).asMilliseconds() * 100;
+      var classes = ["progress-bar-success", "progress-bar", "progress-bar-striped", "active"];
+      classes[0] = b.percent > b.elapsed ? "progress-bar-warning" : classes[0];
+      b.class = classes.join(" ");
+      b.display = b.spent.toFixed() + " of " + b.amount + " this " + b.period;
     });
+    $scope.budgets = data;
+  });
 }
-addDays =function addDays(date, days) {
-        var result = new Date(date);
-        result.setDate(date.getDate() + days);
-        return result;
-    };
+addDays = function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(date.getDate() + days);
+  return result;
+};
 
 getFirstWeekday = function(dayOfWeek, day, month, year) {
-    //gets the first specified weekday following the given month, year, day
-    var myDate = new Date();
-    myDate.setHours(0, 0, 0, 0);
-    myDate.setYear(year);
-    myDate.setDate(day);
-    myDate.setMonth(month);
-    // Find day of week.
-    while (myDate.getDay() !== dayOfWeek) {
-        myDate.setDate(myDate.getDate() + 1);
-    }
-    return myDate;
+  //gets the first specified weekday following the given month, year, day
+  var myDate = new Date();
+  myDate.setHours(0, 0, 0, 0);
+  myDate.setYear(year);
+  myDate.setDate(day);
+  myDate.setMonth(month);
+  // Find day of week.
+  while (myDate.getDay() !== dayOfWeek) {
+    myDate.setDate(myDate.getDate() + 1);
+  }
+  return myDate;
 };
 
 // Should eventually replace server-side code for calculating a user's transactions
 // Both for sidebar display and calculation of user's total daily and weekly spending values
 // Currently used only by getFav method
-function getTrans(bals,format) {
+function getTrans(bals, format) {
   var arr = [];
   for (var i = 0; i < bals.length; i++) {
     if (bals[i + 1]) {
@@ -172,29 +170,29 @@ function getTrans(bals,format) {
 
 // return array of arrays [[cost,freq],[cost2,freq2],...]
 function getFreqs(trans) {
-  var freqs = [[0,0]];
+  var freqs = [[0, 0]];
   // get frequency of each amount spent in trans
   // trans comes in the form [{"amount":"-12.29","date":"2015-10-25T15:01:32.272Z"},{"amount":"-11.60","date":"2015-10-24T18:39:16.613Z"}]
-  trans.forEach(function(x){
+  trans.forEach(function(x) {
     var found = false;
-    for (var i = 0; i<freqs.length; i++){
-      if (freqs[i][0] === x.amount){
+    for (var i = 0; i < freqs.length; i++) {
+      if (freqs[i][0] === x.amount) {
         freqs[i][1]++;
         found = true;
       }
     }
-    if (!found) freqs.push([x.amount,1]);
+    if (!found) freqs.push([x.amount, 1]);
   });
   return freqs;
 }
 
 // sort array of arrays [[costA,freqhighest],[costB,freqnexthighest],...] and return top n
-function getFav(n,trans) {
+function getFav(n, trans) {
   var freqs = getFreqs(trans);
-  freqs.sort(function(a,b){
+  freqs.sort(function(a, b) {
     return b[1] - a[1];
   });
-  return _.filter(freqs.slice(0,n),function(x){
+  return _.filter(freqs.slice(0, n), function(x) {
     return x[1] > 1; // we don't want frequencies of 0 and 1 in the top items.
   });
 }
